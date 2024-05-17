f563bf2f9829864874c214f5a6558f76142cef8d
AnonymousAN21 <109024806+AnonymousAN21@users.noreply.github.com>
2024-05-16 20:16:09 -0700

    Add files via upload

---
+++ WiclifeBio.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Content Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="styleAndrew.css">
+</head>
+<body>
+    <!-- Header -->
+    <header class="header">
+        <a href="" class="logo">BioGrafi</a>
+
+        <i class="fa-solid fa-bars" id="menu-icon"></i>
+        <nav class="navbar">
+            <a href="#home" class="active">Home</a>
+            <a href="#about" >About</a>
+            <a href="#services" >Services</a>
+            <a href="#gallery" >Gallery</a>
+            <a href="#contact" >Contact</a>    
+        </nav>
+    </header>
+
+    <!-- Home Section -->
+    <section class="home" id="home">
+        <div class="home-content">
+            <h3>Hi, I'm</h3>
+            <h1>Wiclife Ganteng</h1>
+            <h3>Drawing <span>Artist</span></h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, exercitationem obcaecati corrupti numquam nihil id illum, praesentium officia vitae possimus, expedita sequi. Eligendi repellendus voluptate doloremque error quos deserunt repudiandae?</p>
+            <div class="social-media">
+                <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+            </div>
+            <a href="#" class="btn">Download CV</a>
+        </div>
+
+        <div class="home-img">
+            <img src="image/WiclifeLogo.png" alt="">
+        </div>
+    </section>
+
+    <!-- about Section -->
+
+    <section class="about" id="about">
+        <div class="about-img">
+            <img src="image/WiclifeLogo.png" alt="">
+        </div>
+
+        <div class="about-content">
+            <h2 class="heading">ABOUT<span>Me</span></h2>
+            <h3>Game Developer</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla id impedit, magni quisquam quo ab alias voluptatibus reiciendis repudiandae dolore. Ea saepe reiciendis minus obcaecati error aspernatur minima odio omnis! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo eum hic quidem doloremque eos in fugiat distinctio accusantium itaque vero perferendis temporibus aut nulla unde, dolorem, ratione deleniti. Quis, debitis.</p>
+            <a href="#" class="btn">Read More</a>
+        </div>
+    </section>
+
+    <!-- Services Section -->
+    <section class="services" id="services">
+        <h2 class="heading">My <span>Services</span></h2>
+
+        <div class="services-container">
+            <div class="services-box">
+                <i class="fa-solid fa-code"></i>
+                <h3>Game Developer</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere esse qui obcaecati et omnis temporibus mollitia voluptatum doloremque eos tempore cumque aspernatur, tempora beatae, voluptas odio ab. Nisi, impedit veritatis.</p>
+                <a href="" class="btn">Read More</a>
+            </div>
+
+            <div class="services-box">
+                <i class="fa-solid fa-code"></i>
+                <h3>Game Developer</h3>
+                <p>Pria ini adalah salah satu orang paling ganteng di dunia saat ini</p>
+                <a href="" class="btn">Read More</a>
+            </div>
+            
+            <div class="services-box">
+                <i class="fa-solid fa-code"></i>
+                <h3>Game Developer</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere esse qui obcaecati et omnis temporibus mollitia voluptatum doloremque eos tempore cumque aspernatur, tempora beatae, voluptas odio ab. Nisi, impedit veritatis.</p>
+                <a href="" class="btn">Read More</a>
+            </div>
+        </div>
+    </section>
+
+    <script src="script.js"></script>
+</body>
+</html>
